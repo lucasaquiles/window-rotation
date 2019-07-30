d54d31b9b31b7b3b0da6a636dc3e09ec31b92b36
Lucas Aquiles <lucasaquiles@gmail.com>
2019-07-30 17:15:06 -0300

    first commit

---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.runtime.onInstalled.addListener(function() {
+	// Remover as regras de mudança de página:
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    	// Para adicionar a regra abaixo:
+        chrome.declarativeContent.onPageChanged.addRules([{
+        	// Se:
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                	// A url da página conter:
+                    pageUrl: { urlContains: 'mail.google.com' },
+                }) ],
+			// Nesse caso, mostramos a Page Action da extensão:
+            actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        }]); 
+    });
+});
---
+++ troll.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	console.log("deu certo");
+
+	jQuery('div').attr('style','-moz-transform: rotate(180deg); -webkit-transform: rotate(180deg); -ms-transform: rotate(180deg);-o-transform: rotate(180deg);   transform: rotate(180deg);');
+
+});
